de9ebcb388ee8f965c56fa711a4e4a1a9522a505
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-08 14:59:17 +0000

    Update nav bar to show register/ login links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  
+  get "about", to: "about#index"
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
