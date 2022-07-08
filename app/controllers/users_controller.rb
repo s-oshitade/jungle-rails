0171d7d8d41cd2050a99a8f20c76ea7842956dc5
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-08 13:33:05 +0000

    Add actions for rendering signup form and creating user on submission of form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
 
+  def new #action for rendering the signup form
+  end
+
+  def create #for receiving the submitted form and creating a user with the form's parameters
+  end
+
 end
