9e7471602ff1d63b30c3ea595c4c1ab5e56ddd42
Seun Oshitade <seun.oshitade@gmail.com>
2022-07-18 21:19:01 +0000

    Add Product model validation tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :price, presence: true
+  validates :price_cents, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    before do
+      @category = Category.new(:name => "Vegetables")
+    end
+    it "Saves a new product if name, price, quantity, and category fields are set" do
+      @product = Product.new(:name => "Cabbage", :price => 199, :quantity => 10, :category => @category)
+      expect(@product).to be_valid
+    end
+    it "Does not save a new product if the name field is not set" do
+      @product = Product.new(:name => nil, :price => 199, :quantity => 10, :category => @category)
+      expect(@product).to_not be_valid
+    end
+    it "Does not save a new product if the price field is not set" do
+      @product = Product.new(:name => "Cabbage", :price_cents => nil, :quantity => 10, :category => @category)
+      expect(@product).to_not be_valid
+    end
+    it "Does not save a new product if the quantity field is not set" do
+      @product = Product.new(:name => "Cabbage", :price => 199, :quantity => nil, :category => @category)
+      expect(@product).to_not be_valid
+    end
+    it "Does not save a new product if the category field is not set" do
+      @product = Product.new(:name => "Cabbage", :price => 199, :quantity => 10, :category => nil)
+      expect(@product).to_not be_valid
+    end
+  end
+end
